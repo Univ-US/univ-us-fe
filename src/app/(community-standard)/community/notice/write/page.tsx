cea97672e884c0a329ee96078f5211606c7346e7
sanryon <wjdtjdfyd17@naver.com>
2026-06-06 20:51:05 +0900

    fix: write 페이지 useSearchParams Suspense 처리

--- src/app/(community-standard)/community/notice/write/page.tsx
+++ src/app/(community-standard)/community/notice/write/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { Suspense } from "react";
 import CommunityBoardWrite from "@/components/common/CommunityBoardWrite";
 
 export default function NoticeBoardWritePage() {
-  return <CommunityBoardWrite board="notice" />;
+  return (
+    <Suspense>
+      <CommunityBoardWrite board="notice" />
+    </Suspense>
+  );
 }
